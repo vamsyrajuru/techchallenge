82ec106f1e5c7a7f4b0171d2a6c457e8ea5fe5f2
Vamsydhar Rajuru <vamsy.rajuru@gmail.com>
2024-05-07 20:37:30 -0400

    New Changes

--- provider.tf
+++ provider.tf
@@ -1,7 +1,5 @@
 provider "aws" {
 #  profile    = "cred_proc"
-  access_key = "AKIAQ65NV2O5CBXEEYWD"
-  secret_key = "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"
   region     = var.aws_region
 }
 
--- vpc_id/provider.tf
+++ vpc_id/provider.tf
@@ -1,5 +1,3 @@
 provider "aws" {
-  access_key = "AKIAQ65NV2O5CBXEEYWD"
-  secret_key = "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"
   region     = "us-west-2"
 }

293fe93d391c511194ad9fb8d31075e967053a79
Vamsydhar Rajuru <vamsy.rajuru@gmail.com>
2024-05-07 21:35:26 -0400

    New Changes

--- provider.tf
+++ provider.tf
@@ -7,3 +7,19 @@ provider "aws" {
 provider "kubernetes" {
   config_path    = "~/.kube/config"
 }
+
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+    }
+  }
+
+  backend "s3" {
+    bucket         	   = "vamsy-rajuru-terraform-state-file-backend"
+    key              	   = "state/terraform.tfstate"
+    region         	   = "us-west-2"
+    encrypt        	   = true
+    dynamodb_table = "tf_lockid"
+  }
+}
